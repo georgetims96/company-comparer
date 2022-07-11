87a3bf37911fcf388b925c61a916127aed3bdd87
George Tims <george.tims@hotmail.com>
2022-07-10 22:20:09 -0400

    added A/P to front end

--- company-comparer-fe/src/financials/CashFlowStatement.js
+++ company-comparer-fe/src/financials/CashFlowStatement.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CashFlowStatement(props) {
     // TODO These should probably be in a config file
-    const possibleFields = ["cfo", "da", "sbc", "ar_delta", "inv_delta", "dr_delta", "cfi", "ppe", "acq"];
+    const possibleFields = ["cfo", "da", "sbc", "ar_delta", "inv_delta", "ap_delta", "dr_delta", "cfi", "ppe", "acq"];
     // Maps JSON fields to appropriate user-friendly name
     const fieldFormatting = {
         "cfo": {"text": "CFO"},
@@ -10,6 +10,7 @@ export default function CashFlowStatement(props) {
         "sbc": {"text": "SBC"},
         "ar_delta": {"text": "Δ A/R"},
         "inv_delta": {"text": "Δ Inv."},
+        "ap_delta": {"text": "Δ A/P"},
         "dr_delta": {"text": "Δ D/R", "style": ""},
         "cfi": {"text" : "CFI"},
         "ppe": {"text": "PPE"},
